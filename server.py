31af9cae2217503a4813ddb6724cd2108b98ff78
root <tnquang.769@gmail.com>
2020-12-11 02:23:40 +0000

    download file

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ from deepspeech_pytorch.configs.inference_config import ServerConfig
 from deepspeech_pytorch.inference import run_transcribe
 from deepspeech_pytorch.loader.data_loader import SpectrogramParser
 from deepspeech_pytorch.utils import load_model, load_decoder
-from flask import send_file
+from flask import send_file, send_from_directory
 from flask import render_template
 import glob
 app = Flask(__name__)
@@ -25,41 +25,57 @@ import datetime
 import json
 @app.errorhandler(404)
 def page_not_found(e):
-    req = request.path.split("/file/")
-    status = True
-    try:
-        if (req[0] != '' and len(req < 2)):
-            status = False
-    except:
-            status = False
-    
-    if status == False:
-        return {'code': 403, 'msg': "Invalid"}
-    path  = "/".join(req)
-    path = path.replace("//", "/")
-    res = {}
-    res['code'] = 200
-    res['message'] = "index"
-    files_path = glob.glob(path+"/*")
-    if (path==None or len(files_path) < 1):
-            res['code'] = 404
-            res['message'] = 'No file / folder found'
-            return res
-    data = []
-    for child in os.scandir(path):
-        item = {}
-        item['path'] = child.path
-        item['name'] = child.name
-        item['size'] = str( "%.1f" % (child.stat().st_size/ 1024))
-        item['date_access'] = datetime.datetime.fromtimestamp(child.stat().st_atime).strftime('%Y/%m/%d %H:%M:%S')
-        item['date_create'] = datetime.datetime.fromtimestamp(child.stat().st_mtime).strftime('%Y/%m/%d %H:%M:%S')
-        if (os.path.isfile(child.path)):
-            item['type'] = 'File'
-        else:
-            item['type'] = 'Folder'
-        data.append(item)
-    res['message'] = data
-    return render_template('file.html', data = res)
+    request.path = request.path.replace("//", "/")
+    if ("/download/" in request.path):
+        req = request.path.split("/download/")
+        status = True
+        try:
+            if (req[0] != '' and len(req < 2)):
+                status = False
+        except:
+                status = False
+        if status == False:
+            return {'code': 403, 'msg': "Invalid"}
+        filename = req[len(req)-1]
+        path  = "/".join(req)
+        path = path.replace("//", "/")
+        return send_file(path, attachment_filename = filename, as_attachment=True)
+    else:
+        req = request.path.split("/file/")
+        status = True
+        try:
+            if (req[0] != '' and len(req < 2)):
+                status = False
+        except:
+                status = False
+        
+        if status == False:
+            return {'code': 403, 'msg': "Invalid"}
+        path  = "/".join(req)
+        path = path.replace("//", "/")
+        res = {}
+        res['code'] = 200
+        res['message'] = "index"
+        files_path = glob.glob(path+"/*")
+        if (path==None or len(files_path) < 1):
+                res['code'] = 404
+                res['message'] = 'No file / folder found'
+                return res
+        data = []
+        for child in os.scandir(path):
+            item = {}
+            item['path'] = child.path
+            item['name'] = child.name
+            item['size'] = str( "%.1f" % (child.stat().st_size/ 1024))
+            item['date_access'] = datetime.datetime.fromtimestamp(child.stat().st_atime).strftime('%Y/%m/%d %H:%M:%S')
+            item['date_create'] = datetime.datetime.fromtimestamp(child.stat().st_mtime).strftime('%Y/%m/%d %H:%M:%S')
+            if (os.path.isfile(child.path)):
+                item['type'] = 'File'
+            else:
+                item['type'] = 'Folder'
+            data.append(item)
+        res['message'] = data
+        return render_template('file.html', data = res)
 @app.route('/transcribe', methods=['POST'])
 def transcribe_file():
     if request.method == 'POST':
@@ -131,49 +147,42 @@ def index(name):
         data.append(item)
     res['message'] = json.loads(data)
     return render_template('index.html')
-    # for item in os.scandir(dir_path):
-    #     type = ""
-    # data = ""
-    # if isHaveFile:
-    #     data = ","
-    # isDirectory = os.path.isdir(item.path)
-    # if (isDirectory):
-    #     type = "Folder"
-    #     totalFolder +=1
-    # else: 
-    #     type = "File"
-    #     totalFile +=1
-    # data += '{"path":"'+item.path+',"name":"'+item.name+'","size": '+str( "%.1f" % (item.stat().st_size/ 1024))+',"date_access": "'+datetime.datetime.fromtimestamp(item.stat().st_atime).strftime('%Y/%m/%d %H:%M:%S')+'","date_create": "'+datetime.datetime.fromtimestamp(item.stat().st_mtime).strftime('%Y/%m/%d %H:%M:%S')+'","type":"'+str(type)+'"}'
-    # jsonString += data
-    # isHaveFile = True
-    # except:
-    # isSuccess = False
-    # jsonString += '],"files":'+str(totalFile)+',"folders":'+str(totalFolder)+',"status":"'+str(isSuccess)+'"}'
-    # to_json = json.loads(jsonString)
+@app.route('/download',methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        da = request.data.decode()
+        xx = json.loads(da)
+        path = xx['path']
+        filename = xx['name']
+        return send_file(path, attachment_filename = filename, as_attachment=True)
+    else:
+        return "200"
+
+if __name__ == '__main__':
+   app.run(debug = True)
 
 
 @hydra.main(config_name="config")
 def main(cfg: ServerConfig):
-    global model, spect_parser, decoder, config, device
-    config = cfg
-    logging.getLogger().setLevel(logging.DEBUG)
+    # global model, spect_parser, decoder, config, device
+    # config = cfg
+    # logging.getLogger().setLevel(logging.DEBUG)
 
-    logging.info('Setting up server...')
-    device = torch.device("cuda" if cfg.model.cuda else "cpu")
+    # logging.info('Setting up server...')
+    # device = torch.device("cuda" if cfg.model.cuda else "cpu")
 
-    model = load_model(device=device,
-                       model_path=cfg.model.model_path,
-                       use_half=cfg.model.use_half)
+    # model = load_model(device=device,
+    #                    model_path=cfg.model.model_path,
+    #                    use_half=cfg.model.use_half)
 
-    decoder = load_decoder(labels=model.labels,
-                           cfg=cfg.lm)
+    # decoder = load_decoder(labels=model.labels,
+    #                        cfg=cfg.lm)
 
-    spect_parser = SpectrogramParser(audio_conf=model.audio_conf,
-                                     normalize=True)
+    # spect_parser = SpectrogramParser(audio_conf=model.audio_conf,
+    #                                  normalize=True)
 
-    spect_parser = SpectrogramParser(model.audio_conf, normalize=True)
+    # spect_parser = SpectrogramParser(model.audio_conf, normalize=True)
     logging.info('Server initialised')
-    app.url_map.strict_slashes = False
     app.run(host=cfg.host, port=cfg.port, debug=True, use_reloader=False)
 
 
